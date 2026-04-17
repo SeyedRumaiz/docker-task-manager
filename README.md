c9401e3e7b68897e6591f2e255415d48e5c8af16
SeyedRumaiz <mrsrumaiz2007@gmail.com>
2026-04-17 06:53:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dockerized Task Manager 🐳
+
+A full-stack containerized application using:
+
+- FastAPI (Python backend)
+- PostgreSQL database
+- Nginx reverse proxy
+- Docker & Docker Compose
+
+## Run the project
+
+```bash
+docker-compose up --build
---
+++ backend/app.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+import psycopg2
+import os
+
+app = FastAPI()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASS = os.getenv("POSTGRES_PASSWORD")
+
+def get_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS
+    )
+
+@app.get("/")
+def read_root():
+    return {"message": "Task Manager API running!"}
+
+@app.get("/tasks")
+def get_tasks():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY KEY, title TEXT);")
+    cur.execute("SELECT * FROM tasks;")
+    tasks = cur.fetchall()
+    cur.close()
+    conn.close()
+    return {"tasks": tasks}
+
+@app.post("/tasks/{title}")
+def add_task(title: str):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO tasks (title) VALUES (%s);", (title,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return {"message": "Task added"}
